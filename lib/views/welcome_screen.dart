26785a9fdc4ea6881555cbb5e47d5fcfa67262d3
Precious Oladele <Oladelep77@gmail.com>
2023-12-07 20:04:34 +0100

    [add] move to home screen

--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -5,6 +5,7 @@ import 'package:gmu_app/constants/colors.dart';
 import 'package:gmu_app/views/pages/home_screen.dart';
 import 'package:gmu_app/widgets/app_bar_widget.dart';
 import 'package:swipeable_button_view/swipeable_button_view.dart';
+import 'package:page_transition/page_transition.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -18,60 +19,75 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarWidget(
-        title: 'GMU - Gospel everywhere',
-      ),
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/lagoom.png',
-            scale: 1.1,
-          ),
-          Text(
-            'find your favorite,\ngospel artiste here!',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          SizedBox(
-            height: 10.h,
-          ),
-          Text(
-            'time to find your favorite\nmusic gospel song here today!',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          SizedBox(
-            height: 25.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: SwipeableButtonView(
-              buttontextstyle: Theme.of(context).textTheme.bodyMedium,
-              onFinish: () async {
-                Get.to(
-                  transition: Transition.fade,
-                  () => const HomeScreen(),
-                );
-              },
-              onWaitingProcess: () {
-                Future.delayed(
-                  const Duration(seconds: 2),
-                  () {
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                'GMU - Gospel everywhere',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              Image.asset(
+                'assets/images/lagoom.png',
+                scale: 1.1,
+              ),
+              Text(
+                'find your favorite,\ngospel artiste here!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                'time to find your favorite\nmusic gospel song here today!',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: SwipeableButtonView(
+                  isFinished: isFinished,
+                  buttontextstyle: Theme.of(context).textTheme.bodyMedium,
+                  onFinish: () async {
+                    await Navigator.push(
+                      context,
+                      PageTransition(
+                        type: PageTransitionType.fade,
+                        child: const HomeScreen(),
+                      ),
+                    );
                     setState(() {
-                      isFinished = true;
+                      isFinished = false;
                     });
                   },
-                );
-              },
-              activeColor: primaryColor,
-              buttonWidget: const Icon(
-                Icons.arrow_forward_ios_rounded,
-                color: Colors.grey,
+                  onWaitingProcess: () {
+                    Future.delayed(
+                      const Duration(seconds: 2),
+                      () {
+                        setState(() {
+                          isFinished = true;
+                        });
+                      },
+                    );
+                  },
+                  activeColor: primaryColor,
+                  buttonWidget: const Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: Colors.grey,
+                  ),
+                  buttonText: 'Enter the realm',
+                ),
               ),
-              buttonText: 'Enter the realm',
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
