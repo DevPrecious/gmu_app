01a135b66e01429360721f2381ec3d7bbb1e3920
Precious Oladele <Oladelep77@gmail.com>
2023-12-08 09:08:16 +0100

    [add] ontap to roundedbuttonwidget

--- lib/widgets/round_button_widget.dart
+++ lib/widgets/round_button_widget.dart
@@ -5,14 +5,17 @@ class RoundButtonWidget extends StatelessWidget {
     super.key,
     required this.icon,
     required this.color,
+    this.onTap,
   });
 
   final Icon icon;
   final Color color;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onTap,
       child: Container(
         padding: const EdgeInsets.all(25),
         decoration: BoxDecoration(
